b30b8f2a8a2149da0ea34d877c30906ebd260a11
Luan Barcaça <73846225+Barcaca@users.noreply.github.com>
2023-11-12 21:26:13 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Constants for star image source and alt text
+const STAR_IMAGE_SRC = "images/icon-star.svg";
+const STAR_ALT_TEXT = "star";
+
+/**
+ * Adds stars to the specified containers.
+ *
+ * @param {string} containerClass - The class of the containers to which stars will be added.
+ * @param {number} numberOfStars - The number of stars to add to each container.
+ */
+function addStarsToContainers(containerClass, numberOfStars) {
+  // Select all containers with the specified class
+  const starContainers = document.querySelectorAll(`.${containerClass}`);
+
+  // Iterate through each container
+  starContainers.forEach((container) => {
+    // Add the specified number of stars to the container
+    for (let i = 0; i < numberOfStars; i++) {
+      // Create a new image element for the star
+      const starImg = document.createElement("img");
+
+      // Set the star image source and alt text
+      starImg.src = STAR_IMAGE_SRC;
+      starImg.alt = STAR_ALT_TEXT;
+
+      // Append the star image to the container
+      container.appendChild(starImg);
+    }
+  });
+}
+
+// Add 5 stars to containers with the class "intro__stars--5"
+addStarsToContainers("intro__stars--5", 5);
